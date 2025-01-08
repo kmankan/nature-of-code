8032fdc7d85b0c2fde1a5ddbae973d491bf74fa9
mahlen <malink027@gmail.com>
2025-01-08 14:47:28 +1100

    chapter 0

--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 400);
 }
 
+// draw executes the lines of code inside its curly brackets 60 times per second 
+//until the program is stopped or the noLoop() function is called
 function draw() {
-  background(225);
-  circle(400, 400, 100);
+  background(135, 206, 235);
+  if (mouseIsPressed === true) {
+    fill('red');
+  } else {
+    fill(255);
+  }
+  //white circles drawn at mouse position
+  circle(mouseX, mouseY, 100);
 }
