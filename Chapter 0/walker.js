5615192aab6abb2a61139f4c4812cca5e63c3b71
mahlen <malink027@gmail.com>
2025-01-09 18:51:44 +1100

    random walk with direction

--- Chapter 0/walker.js
+++ Chapter 0/walker.js
@@ -11,6 +11,11 @@ function draw() {
   walker.show();
 }
 
+function mouseClicked() {
+  console.log('mouseX', mouseX)
+  console.log('mouseY', mouseY)
+}
+
 class Walker {
   constructor() {
     this.x = width/2;
@@ -20,25 +25,51 @@ class Walker {
   // Objects have methods.
   show() {
     stroke(0);
+    strokeWeight(1)
     point(this.x, this.y);
   }
   
   step() {
     const choice = floor(random(4));
     // move the walker forward, backward or either side depending on random roll
-    switch (choice) {
-      case 0:
-        this.x++;
-        break;
-      case 1:
-        this.x--;
-        break;
-      case 2:
-        this.y++;
-        break;
-      case 3:
-        this.y--;
-        break;
+    // switch (choice) {
+    //   case 0:
+    //     this.x++;
+    //     break;
+    //   case 1:
+    //     this.x--;
+    //     break;
+    //   case 2:
+    //     this.y++;
+    //     break;
+    //   case 3:
+    //     this.y--;
+    //     break;
+    // }
+
+    // grid-based 9-option movement
+    // let xstep = floor(random(3)) - 1;
+    // let ystep = floor(random(3)) - 1;
+
+    // any-directional movement within a circle of 1 unit
+    
+    let xstep = random(-1,1.1);
+    //let xstepLeanRight = xstep < 0 ? xstep + random(0,0.2) : xstep;
+    let ystep = random(-1,1.1);
+    //let ystepLeanRight = ystep > 0 ? ystep + random(0,0.2) : ystep;
+
+    const r = random(1);
+
+    if (r < 0.5) {
+      const dx = mouseX > this.x ? random(0,1) : random(-1,0)
+      const dy = mouseY > this.y ? random(0,1) : random(-1,0)
+      this.x += dx;
+      this.y += dy;
+    } else {
+      const xstep = random(-1,1);
+      const ystep = random(-1,1);
+      this.x += xstep;
+      this.y += ystep;
     }
   }
 }
